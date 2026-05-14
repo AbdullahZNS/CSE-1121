fcf013659de369b17f2f93a866748b9bb5446e1e
Abdullah Al Mamun <abdullahmamun.cse20@gmail.com>
2026-05-15 05:42:06 +0600

    Print 1 to N
    Added a program to print numbers from 1 to N based on user input.

---
+++ Basic problems/Loop/problem-01.c
@@ -0,0 +1,18 @@
+/*Print 1 to N: Input N and print numbers from 1 to N.*/
+
+#include<stdio.h>
+int main()
+{
+int N;
+scanf("%d", &N);
+int i;
+for(i = 1; i <= N; i++)
+  {
+  
+printf("%d\n", i);
+  }
+
+
+  
+return 0;
+}
